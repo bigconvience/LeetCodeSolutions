eca9a4cee3ec85fc04f0de7aba58e74464113484
James Jiang <benpeng.jbp@alibaba-inc.com>
2019-09-12 12:58:03 +0800

    fix trap water

--- src/com/jbp/leetcode/TrappingWater.java
+++ src/com/jbp/leetcode/TrappingWater.java
@@ -1,13 +1,73 @@
 package com.jbp.leetcode;
 
 
+import java.util.Stack;
+
 /**
  * https://zhuanlan.zhihu.com/p/79811305
+ * https://leetcode-cn.com/problems/trapping-rain-water/
  */
 public class TrappingWater {
     public static void main(String[] args) {
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(trap(height));
+        System.out.println(trap2(height));
+    }
+
+
+    public static int trap2(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        Stack<Integer> mVector = new Stack<>();
+        int current = 0;
+        while (current < height.length) {
+            while (!mVector.isEmpty() && height[mVector.peek()] < height[current]) {
+                int bar = height[mVector.peek()];
+                mVector.pop();
+                if (mVector.isEmpty()) {
+                    break;
+                }
+
+                result += (Math.min(height[current], height[mVector.peek()]) - bar) * ( current - mVector.peek() - 1);
+            }
+            mVector.push(current);
+            current++;
+        }
+
+        return result;
+    }
+
+    public static int trap1(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        final int size = height.length;
+        int max = 0;
+        for (int i = 1; i < size; i++) {
+            if (height[i] > height[max]) max = i;
+        }
+
+        int result = 0;
+        for (int i = 0, top = 0; i < max; i++) {
+            if (height[i] > top) {
+                top = height[i];
+            } else {
+               result += top - height[i];
+            }
+        }
+
+        for (int i = size - 1, top = 0; i > max; i--) {
+            if (height[i] > top) {
+                top = height[i];
+            } else {
+                result += top - height[i];
+            }
+        }
+
+        return result;
     }
 
     public static int trap(int[] height) {
@@ -15,7 +75,7 @@ public class TrappingWater {
             return 0;
         }
 
-        int size = height.length;
+       final int size = height.length;
         int[] leftMax = new int[size];
         int[] rightMax = new int[size];
 
