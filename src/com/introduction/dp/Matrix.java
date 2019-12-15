d146948acd39f11a452490f18be0e01e20b74aa0
James Jiang <benpeng.jbp@alibaba-inc.com>
2019-12-15 21:34:18 +0800

    update Matrix chain

--- src/com/introduction/dp/Matrix.java
+++ src/com/introduction/dp/Matrix.java
@@ -4,8 +4,9 @@ import com.jbp.utils.MatrixUtils;
 
 public class Matrix {
     public static void main(String[] args) {
-        new Matrix().case1();
+        new Matrix().case4();
         new Matrix().case2();
+        new Matrix().case3();
     }
 
     public void case1() {
@@ -18,6 +19,82 @@ public class Matrix {
         matrixChainOrder(p);
     }
 
+    public void case3() {
+        int[] p = {5, 10, 3, 12, 5, 50, 6};
+        recursiveMatrixChainOrder(p);
+    }
+
+    public void case4() {
+        int[] p = {5, 10, 3, 12, 5, 50, 6};
+        memorizedMatrixChain(p);
+    }
+
+    public void memorizedMatrixChain(int[] p) {
+        int n = p.length - 1;
+        int[][] m = new int[n + 1][n + 1];
+        int[][] s = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                m[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        lookupChain(p, 1, n, m, s);
+
+        printOptimalParens(s, 1, n);
+        System.out.println("");
+    }
+
+    public int lookupChain(int[] p, int i, int j, int[][] m, int[][] s) {
+        if (m[i][j] < Integer.MAX_VALUE) {
+            return m[i][j];
+        }
+
+        if (i == j) {
+            m[i][j] = 0;
+            return 0;
+        }
+
+        for (int k = i; k < j; k++) {
+            int q = lookupChain(p, i, k, m, s) + lookupChain(p, k + 1, j, m, s) + p[i - 1] * p[k] * p[j];
+            if (q < m[i][j]) {
+                m[i][j] = q;
+                s[i][j] = k;
+            }
+        }
+
+        return m[i][j];
+    }
+
+
+    public void recursiveMatrixChainOrder(int[] p) {
+        int n = p.length - 1;
+        int[][] m = new int[n + 1][n + 1];
+        int[][] s = new int[n + 1][n + 1];
+
+        recursiveMatrixChain(p, 1, n, m, s);
+
+        printOptimalParens(s, 1, n);
+        System.out.println("");
+    }
+
+    public int recursiveMatrixChain(int[] p, int i, int j, int[][] m, int[][] s) {
+        if (i == j) {
+            return 0;
+        }
+
+        m[i][j] = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int q = recursiveMatrixChain(p, i, k, m, s) +
+                    recursiveMatrixChain(p, k + 1, j, m, s) + p[i - 1] * p[k] * p[j];
+            if (q < m[i][j]) {
+                m[i][j] = q;
+                s[i][j] = k;
+            }
+        }
+        return m[i][j];
+    }
+
     public void matrixChainOrder(int[] p) {
         int n = p.length - 1;
         int[][] m = new int[n + 1][n + 1];
@@ -28,7 +105,7 @@ public class Matrix {
                 int j = i + l - 1;
                 m[i][j] = Integer.MAX_VALUE;
                 for (int k = i; k <= j - 1; k++) {
-                    int q = m[i][k] + m[k+1][j] + p[i - 1] * p[k] * p[j];
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
                     if (q < m[i][j]) {
                         m[i][j] = q;
                         s[i][j] = k;
@@ -36,13 +113,14 @@ public class Matrix {
                 }
             }
         }
-        MatrixUtils.print(m);
-        MatrixUtils.print(s);
+//        MatrixUtils.print(m);
+//        MatrixUtils.print(s);
 
         printOptimalParens(s, 1, n);
+        System.out.println("");
     }
 
-    public void printOptimalParens(int[][]s, int i, int j) {
+    public void printOptimalParens(int[][] s, int i, int j) {
         if (i == j) {
             System.out.print("A" + i);
         } else {
